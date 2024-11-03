0af3f4a0e1134d9d1fa9c1317c1a8af77e78aedd
dianaRodriguess <dianarodrigues044@gmail.com>
2024-11-03 02:08:12 -0300

    feat: add new asserts for diff kinds of inputs

--- C/test_validation.c
+++ C/test_validation.c
@@ -3,13 +3,20 @@
 #include "validation.h"
 
 void test_validateName() {
-    assert(validateName("Pedro", 5) == 1);
-    printf("Validate name tests passed\n");
+    assert(validateName("Pedro", 6) == 1);
+    assert(validateName("diana rodrigues da silva", 49) == 1);
+    assert(validateName("Pedro de Alcântara Francisco Antônio João Carl", 49) == 1);
+    assert(validateName("áàâãéêíóôõúçÁÀÂÃÉÊÍÓÔÕÚÇ", 49) == 1);
+    assert(validateName("diana rodria1234567890", 49) == 0);
+    assert(validateName("¹²³£¢¬{[]}§/?°®ŧ←↓→øþ´ªæßðđŋħˀĸł´~º«»©„“”µ•·d ̣°", 49) == 0);
+    assert(validateName("!@#$%¨&*()_+=-´`^~[]{};:.,<>d?/|/'\"", 49) == 0);
+    assert(validateName("", 49) == 0);
+    printf("\nValidate name tests passed\n");
 }
 
 int main() {
-    printf("Running tests...\n");
+    printf("\n-----------------------\nRunning tests...\n");
     test_validateName();
-    printf("All tests passed\n");
+    printf("\nAll tests passed\n");
     return 0;
 }
