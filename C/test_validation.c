9806873fe4fd6892f795b7f1e688f0f63bb69596
dianaRodriguess <dianarodrigues044@gmail.com>
2024-11-03 21:01:42 -0300

    refactor: changed the max limit of characters

--- C/test_validation.c
+++ C/test_validation.c
@@ -4,13 +4,13 @@
 
 void test_validateName() {
     assert(validateName("Pedro", 6) == 1);
-    assert(validateName("diana rodrigues da silva", 49) == 1);
-    assert(validateName("Pedro de Alcântara Francisco Antônio João Carl", 49) == 1);
-    assert(validateName("áàâãéêíóôõúçÁÀÂÃÉÊÍÓÔÕÚÇ", 49) == 1);
-    assert(validateName("diana rodria1234567890", 49) == 0);
-    assert(validateName("¹²³£¢¬{[]}§/?°®ŧ←↓→øþ´ªæßðđŋħˀĸł´~º«»©„“”µ•·d ̣°", 49) == 0);
-    assert(validateName("!@#$%¨&*()_+=-´`^~[]{};:.,<>d?/|/'\"", 49) == 0);
-    assert(validateName("", 49) == 0);
+    assert(validateName("diana rodrigues da silva", 54) == 1);
+    assert(validateName("Pedro de Alcântara Francisco Antônio João Carl", 54) == 1);
+    assert(validateName("áàâãéêíóôõúçÁÀÂÃÉÊÍÓÔÕÚÇ", 54) == 1);
+    assert(validateName("diana rodria1234567890", 54) == 0);
+    assert(validateName("¹²³£¢¬{[]}§/?°®ŧ←↓→øþ´ªæßðđŋħˀĸł´~º«»©„“”µ•·d ̣°", 54) == 0);
+    assert(validateName("!@#$%¨&*()_+=-´`^~[]{};:.,<>d?/|/'\"", 54) == 0);
+    assert(validateName("", 54) == 0);
     printf("\nValidate name tests passed\n");
 }
 
